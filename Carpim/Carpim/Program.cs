2b60585cf4760c34c7629464494a81af18669ba4
Veysel Aydın ERALP <veyselaydineralp@gmail.com>
2024-08-13 11:52:30 +0300

    ÇarpımSonucu

---
+++ Carpim/Carpim/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Carpim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci sayiyi giriniz: ");  //birinci sayiyi kullanıcıdan isteriz.
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("İkinci sayiyi giriniz: ");  //ikinci sayiyi kullanicidan ister.
+            int number2 = int.Parse(Console.ReadLine());
+
+            int sonuc = number1 * number2;  //carpim yapilir
+                                            
+            Console.WriteLine($"Girilen sayilarin carpimi: {sonuc}");
+        }
+    }
+}
